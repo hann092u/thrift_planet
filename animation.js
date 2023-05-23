53d0d7e5d36793711fdafd86b73c192f4ba95a3d
vibegrith <vibe9737@stud.kea.dk>
2023-05-23 16:17:11 +0200

    animation

--- animation.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", sidenVises);
-
-const kul1 = document.querySelector("#cirkel_container");
-
-function sidenVises() {
-  console.log("sidenVises");
-  kul1.classList.add("spin");
-}
