8f2a0a56996e474ca5d072dc0eaccc2d31e19ded
Hanna Israielian <hann092u@stud.kea.dk>
2023-05-31 12:53:16 +0200

    style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://hann092u.github.io",
-  base: "/thrift_planet",
-});
+export default defineConfig();
